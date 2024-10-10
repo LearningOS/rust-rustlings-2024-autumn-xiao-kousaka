f4287d1c989e2e41d712dc07544cc6869305de4e
root <root@LAPTOP-6GVAM8J0>
2024-10-10 20:16:10 +0800

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// 不明所以 非常迷惑
 
 #[cfg(test)]
 mod tests {
@@ -13,9 +13,14 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        match optional_target {
+            Some(word) => assert_eq!(word, target),
+            _ => (),
         }
+        // if let 用于简化match控制流
+        // if let Some(word) = optional_target {
+        //     assert_eq!(word, target);
+        // }
     }
 
     #[test]
@@ -32,7 +37,7 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
